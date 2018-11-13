8a04f0d47e5a0b94b6da5310b7b18bc6bff5bdb2
quantumtom <cornyn@gmail.com>
2018-11-12 16:21:55 -0800

    Update server configuration.

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+// server.js
+// load the things we need
 const express = require('express');
 const app = express();
-const path = require('path');
-const PORT = process.env.PORT || 5000;
 
-// app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
+const PORT = process.env.PORT || 8080;
+
+// set the view engine to ejs
 app.set('view engine', 'ejs');
-// 'home' page
-app.get('/', (req, res) => res.render('pages/index'));
+
+// index page
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
 // about page
-app.get('/about', (req, res) => res.render('pages/about'));
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+app.listen(PORT);
+console.log('8080 is the magic port');
