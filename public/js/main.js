f73c92f2f0791476600e5154291bdab8a9ad1ea4
quantumtom <cornyn@gmail.com>
2018-11-14 13:22:09 -0800

    More updates. Image still not loading.

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,11 @@
-const $ = require('jquery');
-const popper = require('popper.js');
-const Bootstrap = require('bootstrap');
 require('../css/bootstrap.min.css');
 require('../css/custom.css');
 require('../css/extra.css');
+require('../img/marcflennert01md.jpg');
+
+const $ = require('jquery');
+const popper = require('popper.js');
+const Bootstrap = require('bootstrap');
 
 define(function () {
     console.log('main.js loaded');
--- webpack.config.js
+++ webpack.config.js
@@ -27,13 +27,7 @@ module.exports = {
                 ]
             },
             {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
                 use: [
                     'file-loader'
                 ]
