5a6f9be1143294b933d6c64e9db8d6e19b807565
quantumtom <cornyn@gmail.com>
2019-01-15 14:38:55 -0800

    Add font handler plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,6 @@ const config = {
       {
         test: /\.(png|woff|woff2|eot|ttf)$/,
         loader: 'url-loader?limit=100000'
-      },
       {
         test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
